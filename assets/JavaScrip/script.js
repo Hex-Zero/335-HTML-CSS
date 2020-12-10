0da20398295351ba1932d2a2c9626b00e3e8cc69
Hex-Zero <hex0@live.com>
2020-12-10 12:01:49 +0000

    times table

--- assets/JavaScrip/script.js
+++ assets/JavaScrip/script.js
@@ -34,6 +34,15 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * (max - 1) + 1);
 }
 
+function timesTable(timesNumber, maxIterations) {
+  let result = [];
+  for (i = 1; i <= maxIterations; i++) {
+    result.push(timesNumber * i);
+  }
+
+  return result;
+}
+
 $(".input-group-prepend.max-calculate-circumference").click(function () {
   $("#basic-addon2").html(
     calculateCircumference($("#max-circumference-input").val())
@@ -46,4 +55,19 @@ $(".input-group-prepend.max-calculate-random").click(function () {
 
 $(".input-group-prepend.max-calculate-squareRoot").click(function () {
   $("#basic-addon3").html(Math.sqrt($("#max-squareRoot-input").val()));
+});
+
+$(".input-group-prepend.max-calculate-timesTable").click(function () {
+  const result = timesTable(
+    $("#max-times-number-input").val(),
+    $("#max-max-iteration-input").val()
+  );
+  $(".max-display-times-results").children().remove();
+  result.forEach((element, index) => {
+    $(".max-display-times-results").append(
+      `<div>${index + 1} x ${$(
+        "#max-times-number-input"
+      ).val()} = ${element}</div>`
+    );
+  });
 });
