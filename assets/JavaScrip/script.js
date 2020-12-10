c82f6e5e940f47d8623ddcbeff8b53ba9567ec71
Hex-Zero <hex0@live.com>
2020-12-10 08:54:56 +0000

    Colour swap new logo

--- assets/JavaScrip/script.js
+++
@@ -1,9 +0,0 @@
-const logo = document.getElementById("max-bird-logo");
-
-function sayBonanza() {
-  alert("Bonanza!!!");
-}
-
-logo.addEventListener("click", () => sayBonanza());
-
-$(".carousel-item").click(() => sayBonanza());
